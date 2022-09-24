3088bb5bb09aeb47569b3df2356b7c0e3347882f
khunzinshine <khunzinshine@gmail.com>
2022-09-24 16:36:34 +0630

    demostrate ngIf, ngStyle, ngClass

---
+++ src/app/test/test.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-test',
+  templateUrl: './test.component.html',
+  styleUrls: ['./test.component.css'],
+})
+export class TestComponent {
+  show: boolean = false;
+  count: any = [];
+
+  onShowDetail() {
+    this.show = !this.show;
+    this.count.push(this.count.length + 1);
+  }
+}
